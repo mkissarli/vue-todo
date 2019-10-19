367c6d430b14f192ea3cb98522bf26b43f5335af
Malik Kissarli <m.kissarli@lancaster.ac.uk>
2019-10-19 15:09:38 +0100

    Made the login pretty.

--- src/Login.vue
+++ src/Login.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="login" class="">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto justify-center w-full max-w-xs">
+  <div id="login" class>
+    <form
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto justify-center w-full max-w-xs"
+    >
       <h2>Login</h2>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
@@ -15,7 +17,7 @@
       <div class="mb-6">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
         <input
-          class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           v-bind:class="{ 'border-red-500': noPassword }"
           id="password"
           type="password"
@@ -23,9 +25,21 @@
           v-model="password"
         />
         <p
+          class="hidden"
+          v-bind:class="{'invisible': (!noPassword || !errorLoggingIn || !errorSigningUp)}"
+        >You discovered an easter egg.</p>
+        <p
           class="text-red-500 text-xs italic"
-          v-bind:class="{ 'visible': noPassword }"
+          v-bind:class="{ 'hidden': !noPassword }"
         >Please choose a password.</p>
+        <p
+          class="text-red-500 text-xs italic"
+          v-bind:class="{ 'hidden': !errorLoggingIn}"
+        >Username or password are incorrect.</p>
+        <p
+          class="text-red-500 text-xs italic"
+          v-bind:class="{ 'hidden': !errorSigningUp}"
+        >That user already exists.</p>
       </div>
       <div class="flex items-center justify-between">
         <button
@@ -58,12 +72,13 @@ export default class Login extends Vue {
   errorSigningUp: boolean = false;
 
   async doLogin(username: string, password: string) {
-    if (password == "") {
+    if (password === undefined || password.trim() === "") {
       this.noPassword = true;
     } else {
       var response = (await userApi.loginUser(username, password)).data.data
         .loginUser;
       var token = response.token;
+      console.log(response);
       if (response.success) {
         this.errorLoggingIn = false;
         this.errorSigningUp = false;
@@ -79,13 +94,12 @@ export default class Login extends Vue {
         this.errorLoggingIn = true;
         this.errorSigningUp = false;
         this.noPassword = false;
-
       }
     }
   }
 
   async doSignup(username: string, password: string) {
-    if (password == "") {
+    if (password === undefined || password.trim() === "") {
       this.noPassword = true;
     } else {
       var response = (await userApi.addUser(username, password)).data.data
@@ -106,7 +120,6 @@ export default class Login extends Vue {
         this.errorLoggingIn = false;
         this.errorSigningUp = true;
         this.noPassword = false;
-
       }
     }
   }
